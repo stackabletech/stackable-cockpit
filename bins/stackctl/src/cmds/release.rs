f82823be512957e02aca499f89ca297ab6e8cd4e
Techassi <git@techassi.dev>
2023-04-17 10:21:58 +0200

    Add missing release subcommands

--- bins/stackctl/src/cmds/release.rs
+++ bins/stackctl/src/cmds/release.rs
@@ -1,7 +1,8 @@
 use clap::{Args, Subcommand};
+use stackable::constants::DEFAULT_LOCAL_CLUSTER_NAME;
 use thiserror::Error;
 
-use crate::cli::OutputType;
+use crate::cli::{ClusterType, OutputType};
 
 #[derive(Debug, Args)]
 pub struct ReleaseArgs {
@@ -17,15 +18,15 @@ pub enum ReleaseCommands {
 
     /// Print out detailed release information
     #[command(alias("desc"))]
-    Describe,
+    Describe(ReleaseDescribeArgs),
 
     /// Install a specific release
     #[command(alias("i"), alias("in"))]
-    Install,
+    Install(ReleaseInstallArgs),
 
     /// Uninstall a release
     #[command(alias("rm"), alias("un"))]
-    Uninstall,
+    Uninstall(ReleaseUninstallArgs),
 }
 
 #[derive(Debug, Args)]
@@ -34,20 +35,69 @@ pub struct ReleaseListArgs {
     output_type: OutputType,
 }
 
+#[derive(Debug, Args)]
+pub struct ReleaseDescribeArgs {
+    #[arg(short, long = "output", value_enum, default_value_t = Default::default())]
+    output_type: OutputType,
+}
+
+#[derive(Debug, Args)]
+pub struct ReleaseInstallArgs {
+    /// Whitelist of product operators to install
+    #[arg(short, long = "include", group = "products")]
+    included_products: Vec<String>,
+
+    /// Blacklist of product operators to install
+    #[arg(short, long = "exclude", group = "products")]
+    excluded_products: Vec<String>,
+
+    /// Type of local cluster to use for testing
+    #[arg(short, long, value_enum, value_name = "CLUSTER_TYPE")]
+    #[arg(
+        long_help = "If specified, a local Kubernetes cluster consisting of 4 nodes (1 for
+control-plane and 3 workers) will be created for testing purposes. Currently
+'kind' and 'minikube' are supported. Both require a working Docker
+installation on the system."
+    )]
+    cluster: ClusterType,
+
+    /// Name of the local cluster
+    #[arg(long, default_value = DEFAULT_LOCAL_CLUSTER_NAME)]
+    cluster_name: String,
+}
+
+#[derive(Debug, Args)]
+pub struct ReleaseUninstallArgs {
+    /// Name of the release to uninstall
+    release_name: String,
+}
+
 #[derive(Debug, Error)]
 pub enum ReleaseError {}
 
 impl ReleaseArgs {
-    pub(crate) fn run(&self) -> Result<String, ReleaseError> {
+    pub fn run(&self) -> Result<String, ReleaseError> {
         match &self.subcommand {
-            ReleaseCommands::List(args) => self.list_cmd(args),
-            ReleaseCommands::Describe => todo!(),
-            ReleaseCommands::Install => todo!(),
-            ReleaseCommands::Uninstall => todo!(),
+            ReleaseCommands::List(args) => list_cmd(args),
+            ReleaseCommands::Describe(args) => describe_cmd(args),
+            ReleaseCommands::Install(args) => install_cmd(args),
+            ReleaseCommands::Uninstall(args) => uninstall_cmd(args),
         }
     }
+}
 
-    fn list_cmd(&self, args: &ReleaseListArgs) -> Result<String, ReleaseError> {
-        todo!()
-    }
+fn list_cmd(args: &ReleaseListArgs) -> Result<String, ReleaseError> {
+    todo!()
+}
+
+fn describe_cmd(args: &ReleaseDescribeArgs) -> Result<String, ReleaseError> {
+    todo!()
+}
+
+fn install_cmd(args: &ReleaseInstallArgs) -> Result<String, ReleaseError> {
+    todo!()
+}
+
+fn uninstall_cmd(args: &ReleaseUninstallArgs) -> Result<String, ReleaseError> {
+    todo!()
 }
