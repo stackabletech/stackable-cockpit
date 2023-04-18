04cb76c63320a54cdc9ac9b4ac2480404736e231
Techassi <git@techassi.dev>
2023-04-18 11:07:28 +0200

    Add runtime shell completions generation

--- bins/stackctl/src/cmds/completions.rs
+++ bins/stackctl/src/cmds/completions.rs
@@ -1,14 +1,47 @@
-use clap::Args;
+use clap::{Args, CommandFactory, Subcommand};
+use clap_complete::{generate, Shell};
 use thiserror::Error;
 
+use crate::cli::Cli;
+
 #[derive(Debug, Args)]
-pub struct CompletionsArgs {}
+pub struct CompletionsArgs {
+    #[command(subcommand)]
+    subcommand: CompletionCommands,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum CompletionCommands {
+    /// Generate shell completions for Bash
+    Bash,
+
+    /// Generate shell completions for Fish
+    Fish,
+
+    /// Generate shell completions for ZSH
+    Zsh,
+}
 
 #[derive(Debug, Error)]
-pub enum CompletionsError {}
+pub enum CompletionsError {
+    #[error("string error: {0}")]
+    StringError(#[from] std::string::FromUtf8Error),
+}
 
 impl CompletionsArgs {
-    pub(crate) fn run(&self) -> Result<String, CompletionsError> {
-        todo!()
+    pub fn run(&self) -> Result<String, CompletionsError> {
+        match &self.subcommand {
+            CompletionCommands::Bash => generate_completions(Shell::Bash),
+            CompletionCommands::Fish => generate_completions(Shell::Fish),
+            CompletionCommands::Zsh => generate_completions(Shell::Zsh),
+        }
     }
+}
+
+fn generate_completions(shell: Shell) -> Result<String, CompletionsError> {
+    let mut cmd = Cli::command();
+    let mut buf = Vec::new();
+
+    generate(shell, &mut cmd, "stackablectl", &mut buf);
+    Ok(String::from_utf8(buf)?)
 }
