9bf053b80680af706e059b5b222e9ddee9a8dd22
Techassi <git@techassi.dev>
2023-04-18 14:48:58 +0200

    Add dotenvy to load .env files

--- bins/stackctl/src/main.rs
+++ bins/stackctl/src/main.rs
@@ -1,13 +1,26 @@
 use anyhow::Result;
 use clap::Parser;
+use dotenvy::dotenv;
 
 use crate::cli::Commands;
 
 pub mod cli;
 pub mod cmds;
+pub mod utils;
 
-fn main() -> Result<()> {
+#[tokio::main]
+async fn main() -> Result<()> {
     let cli = cli::Cli::parse();
+
+    // Load env vars from optional .env file
+    match dotenv() {
+        Ok(_) => {}
+        Err(err) => {
+            if !err.not_found() {
+                return Err(err.into());
+            }
+        }
+    }
 
     let output = match cli.subcommand {
         Commands::Operator(args) => args.run()?,
