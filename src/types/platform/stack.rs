3300163f33cb49df1e7aa3e5ab328dd84675fec6
Techassi <git@techassi.dev>
2023-04-21 10:14:53 +0200

    Harden RawStackParameter parsing, add tests

--- src/types/platform/stack.rs
+++ src/types/platform/stack.rs
@@ -1,13 +1,17 @@
-use std::str::FromStr;
+use std::{
+    fmt::{Display, Formatter, Write},
+    str::FromStr,
+};
 
 use thiserror::Error;
 
+#[derive(Debug, PartialEq)]
 pub struct RawStackParameter {
     pub name: String,
     pub value: String,
 }
 
-#[derive(Debug, Error)]
+#[derive(Debug, Error, PartialEq)]
 pub enum RawStackParameterParseError {
     #[error("invalid equal sign count in stack parameter, expected one")]
     InvalidEqualSignCount,
@@ -15,8 +19,17 @@ pub enum RawStackParameterParseError {
     #[error("invalid stack parameter value, cannot be empty")]
     InvalidParameterValue,
 
+    #[error("invalid stack parameter name, cannot be empty")]
+    InvalidParameterName,
+
     #[error("invalid (empty) stack parameter input")]
     InvalidParameterInput,
+}
+
+impl Display for RawStackParameter {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}={}", self.name, self.value)
+    }
 }
 
 impl FromStr for RawStackParameter {
@@ -43,6 +56,11 @@ impl FromStr for RawStackParameter {
         // Only specifying a key is not valid
         if len == 1 {
             return Err(RawStackParameterParseError::InvalidParameterValue);
+        }
+
+        // If there is an equal sign, but no key before
+        if parts[0].is_empty() {
+            return Err(RawStackParameterParseError::InvalidParameterName);
         }
 
         // If there is an equal sign, but no value after
@@ -74,6 +92,17 @@ impl TryFrom<&str> for RawStackParameter {
 }
 
 pub struct RawStackParameters(Vec<RawStackParameter>);
+
+impl Display for RawStackParameters {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        // We sadly cannot use .join() here, thats why we need to do it the manual way...
+        let mut s = String::new();
+        for param in &self.0 {
+            write!(&mut s, "{} ", param)?;
+        }
+        write!(f, "{}", s.trim_end())
+    }
+}
 
 impl FromStr for RawStackParameters {
     type Err = RawStackParameterParseError;
@@ -121,6 +150,16 @@ impl RawStackParameters {
             index: 0,
         }
     }
+
+    /// Returns the number of parameters
+    pub fn len(&self) -> usize {
+        self.0.len()
+    }
+
+    /// Returns if there are no parameters
+    pub fn is_empty(&self) -> bool {
+        self.0.is_empty()
+    }
 }
 
 pub struct RawStackParameterIter<'a> {
@@ -145,7 +184,9 @@ impl<'a> Iterator for RawStackParameterIter<'a> {
 
 #[cfg(test)]
 mod test {
-    use crate::types::platform::RawStackParameter;
+    use crate::types::platform::{
+        RawStackParameter, RawStackParameterParseError, RawStackParameters,
+    };
 
     #[test]
     fn single_parameter_str() {
@@ -164,6 +205,114 @@ mod test {
             Ok(param) => {
                 assert_eq!(param.name, "param".to_string());
                 assert_eq!(param.value, "value".to_string());
+            }
+            Err(err) => panic!("{err}"),
+        }
+    }
+
+    #[test]
+    fn single_parameter_no_value() {
+        match RawStackParameter::try_from("param") {
+            Ok(param) => panic!("SHOULD FAIL: {param}"),
+            Err(err) => assert_eq!(err, RawStackParameterParseError::InvalidParameterValue),
+        }
+    }
+
+    #[test]
+    fn single_parameter_equal_sign_no_value() {
+        match RawStackParameter::try_from("param=") {
+            Ok(param) => panic!("SHOULD FAIL: {param}"),
+            Err(err) => assert_eq!(err, RawStackParameterParseError::InvalidParameterValue),
+        }
+    }
+
+    #[test]
+    fn single_parameter_only_equal_sign() {
+        match RawStackParameter::try_from("=") {
+            Ok(param) => panic!("SHOULD FAIL: {param}"),
+            Err(err) => assert_eq!(err, RawStackParameterParseError::InvalidParameterName),
+        }
+    }
+
+    #[test]
+    fn single_parameter_multi_equal_sign() {
+        match RawStackParameter::try_from("param=value=invalid") {
+            Ok(param) => panic!("SHOULD FAIL: {param}"),
+            Err(err) => assert_eq!(err, RawStackParameterParseError::InvalidEqualSignCount),
+        }
+    }
+
+    #[test]
+    fn single_parameter_multi_only_equal_sign() {
+        match RawStackParameter::try_from("==") {
+            Ok(param) => panic!("SHOULD FAIL: {param}"),
+            Err(err) => assert_eq!(err, RawStackParameterParseError::InvalidEqualSignCount),
+        }
+    }
+
+    #[test]
+    fn multi_parameters_str() {
+        match RawStackParameters::try_from("param1=value1 param2=value2") {
+            Ok(params) => {
+                assert_eq!(params.len(), 2);
+                let mut iter = params.iter();
+
+                let p = iter.next();
+                assert!(p.is_some());
+                assert_eq!(
+                    p.unwrap(),
+                    &RawStackParameter {
+                        name: "param1".into(),
+                        value: "value1".into()
+                    }
+                );
+
+                let p = iter.next();
+                assert!(p.is_some());
+                assert_eq!(
+                    p.unwrap(),
+                    &RawStackParameter {
+                        name: "param2".into(),
+                        value: "value2".into()
+                    }
+                );
+
+                let p = iter.next();
+                assert!(p.is_none());
+            }
+            Err(err) => panic!("{err}"),
+        }
+    }
+
+    #[test]
+    fn multi_parameters_string() {
+        match RawStackParameters::try_from("param1=value1 param2=value2".to_string()) {
+            Ok(params) => {
+                assert_eq!(params.len(), 2);
+                let mut iter = params.iter();
+
+                let p = iter.next();
+                assert!(p.is_some());
+                assert_eq!(
+                    p.unwrap(),
+                    &RawStackParameter {
+                        name: "param1".into(),
+                        value: "value1".into()
+                    }
+                );
+
+                let p = iter.next();
+                assert!(p.is_some());
+                assert_eq!(
+                    p.unwrap(),
+                    &RawStackParameter {
+                        name: "param2".into(),
+                        value: "value2".into()
+                    }
+                );
+
+                let p = iter.next();
+                assert!(p.is_none());
             }
             Err(err) => panic!("{err}"),
         }
