bd9dcd2c03493ca4d7223a1aa759f0885375aaea
Techassi <git@techassi.dev>
2023-04-26 10:38:23 +0200

    Simplify List trait / struct
    This commit hugely simplifies the definition and inner workings of list
    of specs, like demos, stacks and releases. The cache handling and loading
    of specs is completely self-contained inside the List struct. It is generic
    over the type of spec.

--- src/common/list.rs
+++ src/common/list.rs
@@ -1,53 +1,125 @@
-use std::{fs, path::PathBuf};
+use std::{fs, marker::PhantomData, path::PathBuf};
 
-use async_trait::async_trait;
+use indexmap::IndexMap;
 use serde::{Deserialize, Serialize};
-use url::{ParseError, Url};
+use thiserror::Error;
+use url::Url;
 
 use crate::utils::{
     path::PathOrUrl,
     read::{read_cached_yaml_data, read_yaml_data, CacheStatus, ReadError},
 };
 
-#[async_trait(?Send)]
-pub trait Listed<L>: Sized
-where
-    L: for<'a> Deserialize<'a> + Serialize,
-{
-    type Error: std::fmt::Display
-        + std::error::Error
-        + From<ParseError>
-        + From<ReadError>
-        + From<serde_yaml::Error>
-        + From<std::io::Error>;
+pub trait SpecIter<S> {
+    fn inner(&self) -> &IndexMap<String, S>;
+}
 
-    async fn build<U, T>(
+#[derive(Debug, Error)]
+pub enum ListError {
+    #[error("io error: {0}")]
+    IoError(#[from] std::io::Error),
+
+    #[error("read error: {0}")]
+    ReadError(#[from] ReadError),
+
+    #[error("url parse error: {0}")]
+    ParseUrlError(#[from] url::ParseError),
+
+    #[error("yaml error: {0}")]
+    YamlError(#[from] serde_yaml::Error),
+}
+
+/// A [`List`] describes a list of specs. The list can contain any specs, for example demos, stacks or releases. The
+/// generic parameter `L` represents the initial type of the spec list, directly deserialized from YAML. This type has
+/// to implement [`SpecIter`], which returns a map of specs of type `S`.
+pub struct List<L, S>
+where
+    L: for<'a> Deserialize<'a> + Serialize + SpecIter<S>,
+    S: for<'a> Deserialize<'a> + Serialize + Clone,
+{
+    pub inner: IndexMap<String, S>,
+    list_type: PhantomData<L>,
+}
+
+impl<L, S> List<L, S>
+where
+    L: for<'a> Deserialize<'a> + Serialize + SpecIter<S>,
+    S: for<'a> Deserialize<'a> + Serialize + Clone,
+{
+    pub async fn build<U, T>(
         remote_url: U,
         env_files: T,
         arg_files: T,
         cache_file_path: PathBuf,
         use_cache: bool,
-    ) -> Result<Self, Self::Error>
+    ) -> Result<Self, ListError>
     where
         U: AsRef<str>,
-        T: AsRef<[PathOrUrl]>;
+        T: AsRef<[PathOrUrl]>,
+    {
+        let mut map = IndexMap::new();
+        let remote_url = Url::parse(remote_url.as_ref())?;
+
+        // First load the remote demo file. This uses the cached file if present, and if not, requests the remote file
+        // and then saves the contents on disk for cached use later
+        for (spec_name, spec) in Self::get_default_file(remote_url, cache_file_path, use_cache)
+            .await?
+            .inner()
+        {
+            map.insert(spec_name.clone(), spec.clone());
+        }
+
+        // After that, we load files provided via the ENV var
+        for env_file in env_files.as_ref() {
+            let demos = read_yaml_data::<L>(env_file).await?;
+            for (demo_name, demo) in demos.inner() {
+                map.insert(demo_name.to_owned(), demo.to_owned());
+            }
+        }
+
+        // Lastly, the CLI argument --additional-demo-files is used
+        for arg_file in arg_files.as_ref() {
+            let demos = read_yaml_data::<L>(arg_file).await?;
+            for (demo_name, demo) in demos.inner() {
+                map.insert(demo_name.to_owned(), demo.to_owned());
+            }
+        }
+
+        Ok(Self {
+            list_type: PhantomData,
+            inner: map,
+        })
+    }
+
+    /// Returns a reference to the inner [`IndexMap`]
+    pub fn inner(&self) -> &IndexMap<String, S> {
+        &self.inner
+    }
+
+    /// Returns an optional reference to a single spec of type `S` by `name`
+    pub fn get<T>(&self, name: T) -> Option<&S>
+    where
+        T: Into<String>,
+    {
+        self.inner.get(&name.into())
+    }
 
     async fn get_default_file(
-        url: Url,
+        remote_url: Url,
         cache_file_path: PathBuf,
         use_cache: bool,
-    ) -> Result<L, Self::Error> {
+    ) -> Result<L, ListError> {
         let specs = if use_cache {
             match read_cached_yaml_data::<L>(cache_file_path.clone())? {
                 CacheStatus::Hit(demos) => demos,
                 CacheStatus::Expired | CacheStatus::Miss => {
-                    let demos = read_yaml_data::<L>(url).await?;
+                    let demos = read_yaml_data::<L>(remote_url).await?;
                     fs::write(cache_file_path, serde_yaml::to_string(&demos)?)?;
                     demos
                 }
             }
         } else {
-            read_yaml_data::<L>(url).await?
+            read_yaml_data::<L>(remote_url).await?
         };
 
         Ok(specs)
