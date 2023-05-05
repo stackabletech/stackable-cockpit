69d87a4f3529c5c422acc3b7a08cc9c4429f349d
Natalie Klestrup Röijezon <nat@nullable.se>
2023-05-05 23:17:58 +0200

    Minor styling

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,7 +1,6 @@
+import '@unocss/reset/sanitize/sanitize.css';
 import 'virtual:uno.css';
 import { render } from 'solid-js/web';
-import { Show, For, createResource, Switch, Match } from 'solid-js';
-import { getListeners } from './api';
 import { A, Route, Router, Routes } from '@solidjs/router';
 import { Listeners } from './components/listeners';
 
@@ -11,19 +10,36 @@ const Home = () => {
     </>;
 }
 
-const App = () => {
+const GlobalNav = () => {
+    const NavItem = (props: any) =>
+        <li class='inline mr-1'>
+            <A href={props.href}
+               class="p-1 c-white inline-block"
+               activeClass='bg-stblue'
+               inactiveClass='bg-stblue bg-opacity-50 hover:bg-opacity-80'>
+                {props.children}
+            </A>
+        </li>;
+
     return <>
-        <h1>stackablectl, web edition</h1>
-        <nav>
-            <ul>
-                <li><A href='/listeners'>listeners</A></li>
+        <nav class='bg-gray'>
+            <ul class='m-0 p-0'>
+                <NavItem href="/listeners">listeners</NavItem>
+                <NavItem href="/stacks">stacks</NavItem>
             </ul>
         </nav>
+    </>
+}
+
+const App = () => {
+    return <div class='w-5xl ma'>
+        <h1>stackablectl, web edition</h1>
+        <GlobalNav />
         <Routes>
             <Route path="/listeners" component={Listeners} />
             <Route path="/" component={Home} />
         </Routes>
-    </>
+    </div>
 };
 
 render(() => <Router base="/ui"><App/></Router>, document.getElementById("app")!!);
---
+++ web/uno.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig, presetUno } from 'unocss';
+
+export default defineConfig({
+    theme: {
+        colors: {
+            'stblue': '#1880bd',
+        },
+    },
+    presets: [presetUno()],
+});
