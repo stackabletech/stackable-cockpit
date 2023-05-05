2b24dfa301edef54f3839fab6d762e8ab81ff502
Natalie Klestrup Röijezon <nat@nullable.se>
2023-05-05 11:11:57 +0200

    Hello Uno

--- bins/stackabled/web-ui/src/main.tsx
+++ bins/stackabled/web-ui/src/main.tsx
@@ -1,7 +1,11 @@
+import 'virtual:uno.css';
 import { render } from 'solid-js/web';
+import { createSignal } from 'solid-js';
 
 function MyComponent() {
-    return <div>hi</div>;
+    const [counter, setCounter] = createSignal(0);
+    const increment = () => {setCounter(c => c + 1)};
+    return <div class="c-yellow">hi <button onclick={increment}>{counter()}</button></div>;
 }
 
 render(MyComponent, document.getElementById("app")!!);
