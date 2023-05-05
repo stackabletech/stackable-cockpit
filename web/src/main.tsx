c14db26b2a39d7b0680540fa4b8d82b141bf40b2
Natalie Klestrup Röijezon <nat@nullable.se>
2023-05-05 21:59:26 +0200

    Move listeners into separate component

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,45 +3,7 @@ import { render } from 'solid-js/web';
 import { Show, For, createResource, Switch, Match } from 'solid-js';
 import { getListeners } from './api';
 import { A, Route, Router, Routes } from '@solidjs/router';
-
-const Listeners = () => {
-    const [listeners, { refetch }] = createResource(getListeners);
-    return <>
-        <button onClick={refetch}>Refresh</button>
-        <Show when={listeners.loading}>Loading...</Show>
-        <table>
-            <thead>
-                <tr>
-                    <th>Product</th>
-                    <th>Namespace</th>
-                    <th>Name</th>
-                    <th>Endpoints</th>
-                    <th>Info</th>
-                </tr>
-            </thead>
-            <tbody>
-                <For each={listeners()}>{(listener) =>
-                    <tr>
-                        <td>{listener.product}</td>
-                        <td>{listener.metadata.namespace}</td>
-                        <td>{listener.metadata.name}</td>
-                        <td>
-                            <ul>
-                                <For each={listener.endpoints}>{(endpoint) =>
-                                    <li>
-                                        <Switch fallback={endpoint.path}>
-                                            <Match when={endpoint.web}><a href={endpoint.path}>{endpoint.path}</a></Match>
-                                        </Switch>
-                                    </li>
-                                }</For>
-                            </ul>
-                        </td>
-                    </tr>
-                }</For>
-            </tbody>
-        </table>
-    </>;
-};
+import { Listeners } from './components/listeners';
 
 const Home = () => {
     return <>
