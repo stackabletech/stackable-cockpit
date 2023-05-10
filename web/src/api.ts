f15140690e76763648aeb67482a91e003e01f48b
Techassi <git@techassi.dev>
2023-05-10 10:08:26 +0200

    Apply ESLint pass, add typed NavItem props

--- web/src/api.ts
+++ web/src/api.ts
@@ -1,42 +1,42 @@
 interface ObjectMeta {
-    namespace: string,
-    name: string,
+  namespace: string;
+  name: string;
 }
 
 interface Listener {
-    metadata: ObjectMeta,
-    product: string,
-    endpoints: ListenerEndpoint[],
+  metadata: ObjectMeta;
+  product: string;
+  endpoints: ListenerEndpoint[];
 }
 
 interface ListenerEndpoint {
-    path: string,
-    web: boolean,
+  path: string;
+  web: boolean;
 }
 
 function delay(amount: number): Promise<void> {
-    return new Promise(resolve => setTimeout(resolve, amount));
+  return new Promise(resolve => setTimeout(resolve, amount));
 }
 
 export async function getListeners(): Promise<Listener[]> {
-    await delay(200);
-    return [
+  await delay(200);
+  return [
+    {
+      metadata: {
+        namespace: 'default',
+        name: 'simple-nifi',
+      },
+      product: 'nifi',
+      endpoints: [
         {
-            metadata: {
-                namespace: "default",
-                name: "simple-nifi",
-            },
-            product: "nifi",
-            endpoints: [
-                {
-                    path: "https://127.0.0.1:8443/ui",
-                    web: true,
-                },
-                {
-                    path: "mqtt://127.0.0.1:9999",
-                    web: false,
-                }
-            ],
+          path: 'https://127.0.0.1:8443/ui',
+          web: true,
         },
-    ];
+        {
+          path: 'mqtt://127.0.0.1:9999',
+          web: false,
+        },
+      ],
+    },
+  ];
 }
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,45 +1,59 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import '@unocss/reset/sanitize/sanitize.css';
 import 'virtual:uno.css';
 import { render } from 'solid-js/web';
 import { A, Route, Router, Routes } from '@solidjs/router';
+import type { Component, ParentProps } from 'solid-js';
 import { Listeners } from './components/listeners';
 
 const Home = () => {
-    return <>
-        lorem ipsum dolor sit amet
-    </>;
+  return <>
+    lorem ipsum dolor sit amet
+  </>;
+};
+
+export interface NavItemProps {
+  href: string;
 }
 
 const GlobalNav = () => {
-    const NavItem = (props: any) =>
-        <li class='inline mr-1'>
-            <A href={props.href}
-               class="p-1 c-white inline-block"
-               activeClass='bg-stblue'
-               inactiveClass='bg-stblue bg-opacity-50 hover:bg-opacity-80'>
-                {props.children}
-            </A>
-        </li>;
+  const NavItem: Component<ParentProps<NavItemProps>> = props =>
+    <li class='inline mr-1'>
+      <A href={props.href}
+        class="p-1 c-white inline-block"
+        activeClass='bg-stblue'
+        inactiveClass='bg-stblue bg-opacity-50 hover:bg-opacity-80'>
+        {props.children}
+      </A>
+    </li>;
 
-    return <>
-        <nav class='bg-gray'>
-            <ul class='m-0 p-0'>
-                <NavItem href="/listeners">listeners</NavItem>
-                <NavItem href="/stacks">stacks</NavItem>
-            </ul>
-        </nav>
-    </>
-}
-
-const App = () => {
-    return <div class='w-5xl ma'>
-        <h1>stackablectl, web edition</h1>
-        <GlobalNav />
-        <Routes>
-            <Route path="/listeners" component={Listeners} />
-            <Route path="/" component={Home} />
-        </Routes>
-    </div>
+  return <>
+    <nav class='bg-gray'>
+      <ul class='m-0 p-0'>
+        <NavItem href="/listeners">listeners</NavItem>
+        <NavItem href="/stacks">stacks</NavItem>
+      </ul>
+    </nav>
+  </>;
 };
 
-render(() => <Router base="/ui"><App/></Router>, document.getElementById("app")!!);
+const App = () => {
+  return <div class='w-5xl ma'>
+    <h1>stackablectl, web edition</h1>
+    <GlobalNav />
+    <Routes>
+      <Route path="/listeners" component={Listeners} />
+      <Route path="/" component={Home} />
+    </Routes>
+  </div>;
+};
+
+const root = document.querySelector('#app');
+
+if (import.meta.env.DEV && root === undefined) {
+  throw new Error(
+    'Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got mispelled?',
+  );
+}
+
+render(() => <Router base="/ui"><App /></Router>, root!);
