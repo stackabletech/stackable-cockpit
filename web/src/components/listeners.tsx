7e57ee34a507011da1bcdc9125c2168f8420b04c
Natalie Klestrup Röijezon <nat@nullable.se>
2023-05-11 12:24:50 +0200

    Extract table rendering concerns from listener into a separate component

--- web/src/components/listeners.tsx
+++ web/src/components/listeners.tsx
@@ -1,42 +1,61 @@
-import { Show, For, Switch, Match, createResource } from 'solid-js';
-import { getListeners } from '../api';
+import { Show, For, Switch, Match, createResource, JSX } from "solid-js";
+import { getListeners } from "../api";
 
-// eslint-disable-next-line @typescript-eslint/naming-convention
-export const Listeners = () => {
-  const [listeners, { refetch }] = createResource(getListeners);
-  return <>
-    <button onClick={refetch}>Refresh</button>
-    <Show when={listeners.loading}>Loading...</Show>
-    <table>
-      <thead>
-        <tr>
-          <th>Product</th>
-          <th>Namespace</th>
-          <th>Name</th>
-          <th>Endpoints</th>
-          <th>Info</th>
-        </tr>
-      </thead>
-      <tbody>
-        <For each={listeners()}>{listener =>
-          <tr>
-            <td>{listener.product}</td>
-            <td>{listener.metadata.namespace}</td>
-            <td>{listener.metadata.name}</td>
-            <td>
-              <ul>
-                <For each={listener.endpoints}>{endpoint =>
-                  <li>
-                    <Switch fallback={endpoint.path}>
-                      <Match when={endpoint.web}><a href={endpoint.path}>{endpoint.path}</a></Match>
-                    </Switch>
-                  </li>
+interface DataTableColumn<T> {
+    label: string,
+    get: (x: T) => JSX.Element,
+}
+interface DataTableProps<T> {
+    columns: DataTableColumn<T>[],
+    items: T[],
+}
+function DataTable<T>(props: DataTableProps<T>): JSX.Element {
+    return <>
+        <table>
+            <thead>
+                <tr>
+                    <For each={props.columns}>{(col) =>
+                        <th>
+                            {col.label}
+                        </th>
+                    }</For>
+                </tr>
+            </thead>
+            <tbody>
+                <For each={props.items}>{(item) =>
+                    <tr>
+                        <For each={props.columns}>{(col) =>
+                            <td>{col.get(item)}</td>
+                        }</For>
+                    </tr>
                 }</For>
-              </ul>
-            </td>
-          </tr>
-        }</For>
-      </tbody>
-    </table>
-  </>;
+            </tbody>
+        </table>
+    </>;
+}
+
+export const Listeners = () => {
+    const [listeners, { refetch }] = createResource(getListeners);
+    return <>
+        <button onClick={refetch}>Refresh</button>
+        <Show when={listeners.loading}>Loading...</Show>
+        <DataTable items={listeners() || []} columns={[
+            { label: "Product", get: listener => listener.product },
+            { label: "Namespace", get: listener => listener.metadata.namespace },
+            { label: "Name", get: listener => listener.metadata.name },
+            {
+                label: "Endpoints",
+                get: listener => <ul>
+                    <For each={listener.endpoints}>{(endpoint) =>
+                        <li>
+                            <Switch fallback={endpoint.path}>
+                                <Match when={endpoint.web}><a href={endpoint.path}>{endpoint.path}</a></Match>
+                            </Switch>
+                        </li>
+                    }</For>
+                </ul>
+            },
+            { label: "Info", get: _ => "" }
+        ]} />
+    </>;
 };
