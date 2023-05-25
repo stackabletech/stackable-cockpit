c3139f6c0f7ca56af40694ebacd8083ef4b17b8f
Natalie Klestrup Röijezon <nat@nullable.se>
2023-05-25 12:27:14 +0200

    Enable Vite dev server proxy for stackabled

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,4 +4,10 @@ import UnoCSS from 'unocss/vite';
 
 export default defineConfig({
   plugins: [solidPlugin(), UnoCSS()],
+  server: {
+    proxy: {
+      // Forward API requests to stackabled
+      '/api': 'http://localhost:8000',
+    },
+  },
 });
