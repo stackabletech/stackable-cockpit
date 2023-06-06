23725315c39417da9275ef588d65f1c6db34e370
Techassi <git@techassi.dev>
2023-06-06 14:19:58 +0200

    Add doc comments to Option methods

--- web/src/types/option.ts
+++ web/src/types/option.ts
@@ -31,40 +31,183 @@ interface BaseOption<S> {
     none: () => K;
   }): T | K;
 
+  /**
+   * Maps an `Option<S>` to `Option<T>` by applying a function to a contained
+   * value (if `Some`) or returns `None` (if `None`).
+   *
+   * @param fn Mapping function.
+   * @returns The mapped option.
+   */
   map<T>(fn: (value: Readonly<S>) => T): Option<T>;
 
+  /**
+   * Returns the provided default result (if none), or applies a function to
+   * the contained value (if any).
+   *
+   * @param defaultValue The default value if `None`.
+   * @param fn The mapping function.
+   */
   mapOr<T>(defaultValue: T, fn: (value: Readonly<S>) => T): T;
 
+  /**
+   * Computes a default function result (if none), or applies a different
+   * function to the contained value (if any).
+   *
+   * @param defaultFn Closure computing the default value if `None`.
+   * @param fn The mapping function.
+   */
   mapOrElse<T>(defaultFn: () => T, fn: (value: Readonly<S>) => T): T;
 
+  /**
+   * Transforms the `Option<S>` into a `Result<S, E>`, mapping `Some(v)` to
+   * `Ok(v)` and `None` to `Err(err)`.
+   *
+   * @param error The error value if `None`.
+   * @returns The transformed result.
+   */
   okOr<E extends ToString>(error: E): Result<S, E>;
 
+  /**
+   * Transforms the `Option<S>` into a `Result<S, E>`, mapping `Some(v)` to
+   * `Ok(v)` and `None` to `Err(err())`.
+   *
+   * @param err Closure to compute the error value if `None`.
+   * @returns The transformed result.
+   */
   okOrElse<E extends ToString>(err: () => E): Result<S, E>;
 
+  /**
+   * Returns `None` if the option is `None`, otherwise returns `other`.
+   *
+   * @param other The other option if `Some`.
+   * @returns Either `None` or the `other` option.
+   */
   and<T>(other: Option<T>): Option<T>;
 
+  /**
+   * Returns `None` if the option is `None`, otherwise calls `fn` with the
+   * wrapped value and returns the result.
+   *
+   * @param fn The function which produces the other option if `Some`.
+   * @returns Either `None` or the computed other option.
+   */
   andThen<T>(fn: (value: Readonly<S>) => Option<T>): Option<T>;
 
+  /**
+   * Returns `None` if the option is `None`, otherwise calls `predicate` with
+   * the wrapped value and returns:
+   *
+   * - `Some(t)` if predicate returns true (where t is the wrapped value), and
+   * - `None` if predicate returns false.
+   *
+   * @param predicate The filter function.
+   * @returns `Some` if the filter function returned true, otherwise `None`.
+   */
   filter(predicate: (value: Readonly<S>) => boolean): Option<S>;
 
+  /**
+   * Returns the option if it contains a value, otherwise returns `other`.
+   *
+   * @param other The `other` option if `None`.
+   * @returns Either this or `other`.
+   */
   or<T>(other: Option<S | T>): Option<S | T>;
 
+  /**
+   * Returns the option if it contains a value, otherwise calls `fn` and returns
+   * the result.
+   *
+   * @param fn Function to compute the other option.
+   * @returns Either this or the other computed option.
+   */
   orElse<T>(fn: () => Option<S | T>): Option<S | T>;
 
+  /**
+   * Returns `Some` if exactly one of self, `other` is `Some`, otherwise returns
+   * `None`.
+   *
+   * @param other The other option.
+   */
   xor<T>(other: Option<S | T>): Option<S | T>;
 
+  /**
+   * Returns true if the option is a `Some` value containing the given `value`.
+   *
+   * @param value The value to match `Some(value)`.
+   * @returns boolean
+   */
   contains(value: S): boolean;
 
+  /**
+   * Zips `self` with another `Option`.
+   *
+   * If `self` is `Some(s)` and other is `Some(o)`, this method returns
+   * `Some([s, o])`. Otherwise, `None` is returned.
+   *
+   * @param other The other option to zip with.
+   * @returns The zipped option or `None`.
+   */
   zip<T>(other: Option<T>): Option<[S, T]>;
 
+  /**
+   * Zips `self` and another `Option` with function `fn`.
+   *
+   * If `self` is `Some(s)` and other is `Some(o)`, this method returns
+   * `Some(fn(s, o))`. Otherwise, `None` is returned.
+   *
+   * @param other The other option to zip with.
+   * @param fn Function which get's applied to the zipped values.
+   * @returns The zipped and transformed option or `None`.
+   */
   zipWith<T, K>(other: Option<T>, fn: (self: S, other: T) => K): Option<K>;
 
+  /**
+   * Returns the contained `Some` value.
+   *
+   * Because this function may throw, its use is generally discouraged. Instead,
+   * prefer to use pattern matching and handle the `None` case explicitly, or
+   * call `unwrap_or` or `unwrap_or_else`.
+   *
+   * @returns The contained `Some` value.
+   *
+   * @throws Throws if the self value equals `None`.
+   */
   unwrap(): S;
 
+  /**
+   * Returns the contained `Some` value or a provided default.
+   *
+   * @param defaultValue Default value returned when `None`.
+   * @returns Either contained `Some` value of `defaultValue`.
+   */
   unwrapOr<T>(defaultValue: S | T): S | T;
 
+  /**
+   * Returns the contained `Some` value or computes it from a closure.
+   *
+   * @param fn Closure to compute default value.
+   */
   unwrapOrElse<T>(fn: () => S | T): S | T;
 
+  /**
+   * Returns the contained Some value
+   *
+   * ### Recommended Message Style
+   *
+   * We recommend that `expect` messages are used to describe the reason you
+   * expect the `Option` should be `Some`.
+   *
+   * **Hint:** If you’re having trouble remembering how to phrase expect error
+   * messages remember to focus on the word “should” as in “env variable should
+   * be set by blah” or “the given binary should be available and executable by
+   * the current user”.
+   *
+   * @param message Error message.
+   * @returns The contained `Some` value.
+   *
+   * @throws Throws if the value is a `None` with a custom panic message
+   * provided by `msg`.
+   */
   expect(message: string): S;
 }
 
