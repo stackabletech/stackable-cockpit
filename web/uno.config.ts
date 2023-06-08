9117dc65b9bd745de40cf02432c50044e844b00e
Natalie Klestrup Röijezon <nat@nullable.se>
2023-06-08 18:08:17 +0200

    Implement UI header according to the Figma design

--- web/uno.config.ts
+++ web/uno.config.ts
@@ -4,6 +4,13 @@ export default defineConfig({
   theme: {
     colors: {
       stblue: '#1880bd',
+      'gray-400': '#9CA3AF',
+      'gray-600': '#4B5563',
+      'gray-700': '#374151',
+      'gray-800': '#1F2937',
+      'gray-900': '#111827',
+      'stblue-light': '#1880BD',
+      'stblue-dark': '#0B689F',
     },
   },
   presets: [presetUno()],
