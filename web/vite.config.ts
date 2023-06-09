c88ebd5558be3ce6758f6a2f0f84a23c0df5610f
Natalie Klestrup Röijezon <nat@nullable.se>
2023-06-09 15:28:34 +0200

    The API only listens on IPv4 at the moment

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       // Forward API requests to stackabled
-      '/api': 'http://localhost:8000',
+      '/api': 'http://127.0.0.1:8000',
     },
   },
 });
