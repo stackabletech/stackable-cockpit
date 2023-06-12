d8c6e371ee18f56ade3b6239d2393aaacafa596f
Natalie <nat@nullable.se>
2023-06-12 09:08:19 +0000

    Start connecting the UI to the API (#34)
    # Description
    
    This loads the list of stacklets from a mock endpoint in the API, with a type schema generated by utoipa.
    
    It also fixes up the cases where OpenAPI didn't quite line up with the actual APIs.
    
    Fixes #33, part of #14.

--- bins/stackabled/src/main.rs
+++ bins/stackabled/src/main.rs
@@ -1,32 +1,16 @@
 use std::net::SocketAddr;
 
+use api_doc::ApiDoc;
 use axum::{response::Redirect, routing::get, Router, Server};
 use clap::Parser;
-use stackable::{
-    common::ManifestSpec,
-    platform::{demo::DemoSpecV2, release::ReleaseSpec},
-    utils::params::Parameter,
-};
 use utoipa::OpenApi;
 use utoipa_swagger_ui::SwaggerUi;
 
 use crate::cli::Cli;
 
+mod api_doc;
 mod cli;
 mod handlers;
-
-#[derive(Debug, OpenApi)]
-#[openapi(
-    info(description = "Stackabled API specification"),
-    paths(
-        handlers::get_demos,
-        handlers::get_demo,
-        handlers::get_releases,
-        handlers::get_release
-    ),
-    components(schemas(DemoSpecV2, ManifestSpec, Parameter, ReleaseSpec))
-)]
-struct ApiDoc {}
 
 #[tokio::main]
 async fn main() {
@@ -34,15 +18,16 @@ async fn main() {
 
     // Run the server
     let api = Router::new()
-        .route("/", get(handlers::get_root))
-        .nest("/demos", handlers::demo_router())
-        .nest("/stacks", handlers::stack_router())
-        .nest("/releases", handlers::release_router())
-        .merge(SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", ApiDoc::openapi()));
+        .route("/", get(handlers::root::get_root))
+        .nest("/demos", handlers::demos::router())
+        .nest("/stacks", handlers::stacks::router())
+        .nest("/releases", handlers::releases::router())
+        .nest("/stacklets", handlers::stacklets::router());
 
     let router = Router::new()
         .nest("/api/", api)
         .nest("/ui/", handlers::ui::router())
+        .merge(SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", ApiDoc::openapi()))
         .route("/", get(|| async { Redirect::permanent("/ui/") }));
 
     // Needed in next axum version
