590f24f3d17d23b5678bcfc53dbf290bcbc0a26f
Techassi <git@techassi.dev>
2023-06-15 15:10:00 +0200

    Re-model Result type with subtypes

--- web/src/types/result.ts
+++ web/src/types/result.ts
@@ -1,31 +1,13 @@
 import { None, Option, Some } from './option';
 import { ToString } from './utils';
 
-export const Ok = <O, E extends ToString = never>(value: O) => {
-  return new Result<O, E>(false, value);
-};
-
-export const Err = <E extends ToString, O = never>(error: E) => {
-  return new Result<O, E>(true, error);
-};
-
-export class Result<O, E extends ToString> {
-  private is_error: boolean;
-  private value: O | E;
-
-  constructor(is_error: boolean, value: O | E) {
-    this.is_error = is_error;
-    this.value = value;
-  }
-
+interface BaseResult<O, E extends ToString> {
   /**
    * Returns true if the result is `Ok`.
    *
    * @returns boolean
    */
-  isOk(): this is Result<O, never> {
-    return !this.is_error;
-  }
+  isOk(): this is Ok<O>;
 
   /**
    * Returns `true` if the result is `Ok` and the value inside of it matches
@@ -34,18 +16,14 @@ export class Result<O, E extends ToString> {
    * @param fn Predicate to match.
    * @returns boolean
    */
-  isOkAnd(fn: (value: Readonly<O>) => boolean): this is Result<O, never> {
-    return this.isOk() && fn(this.value);
-  }
+  isOkAnd(fn: (value: Readonly<O>) => boolean): this is Ok<O>;
 
   /**
    * Returns `true` if the result is `Err`.
    *
    * @returns boolean
    */
-  isErr(): this is Result<never, E> {
-    return this.is_error;
-  }
+  isErr(): this is Err<E>;
 
   /**
    * Returns `true` if the result is `Err` and the value inside of it matches
@@ -54,18 +32,12 @@ export class Result<O, E extends ToString> {
    * @param fn Predicate to match.
    * @returns boolean
    */
-  isErrAnd(fn: (error: Readonly<E>) => boolean): this is Result<never, E> {
-    return this.isErr() && fn(this.value);
-  }
+  isErrAnd(fn: (error: Readonly<E>) => boolean): this is Err<E>;
 
   match<T, K>(matcher: {
     ok: (value: Readonly<O>) => T;
     err: (error: Readonly<E>) => K;
-  }): T | K {
-    return this.is_error
-      ? matcher.err(this.value as E)
-      : matcher.ok(this.value as O);
-  }
+  }): T | K;
 
   /**
    * Returns the contained `Ok` value.
@@ -79,9 +51,7 @@ export class Result<O, E extends ToString> {
    * @throws Throws if the value is an `Err`, with a panic message provided by
    * the `Err`'s value.
    */
-  unwrap(): O {
-    return this.expect('called `Result.unwrap()` on an `Err` value');
-  }
+  unwrap(): O;
 
   /**
    * Returns the contained `Err` value.
@@ -91,9 +61,7 @@ export class Result<O, E extends ToString> {
    * @throws Throws if the value is an `Ok`, with a custom panic message
    * provided by the `Ok`'s value.
    */
-  unwrapErr(): E {
-    return this.expectErr('called `Result.unwrapErr()` on an `Ok` value');
-  }
+  unwrapErr(): E;
 
   /**
    * Returns the contained `Ok` value or a provided default.
@@ -101,12 +69,7 @@ export class Result<O, E extends ToString> {
    * @param defaultValue The default value if `Err`.
    * @returns Either contained `Ok` value of default value.
    */
-  unwrapOr(defaultValue: O): O {
-    return this.match({
-      ok: (value) => value,
-      err: () => defaultValue,
-    });
-  }
+  unwrapOr(defaultValue: O): O;
 
   /**
    * Returns the contained `Ok` value or computes it from a closure.
@@ -114,12 +77,7 @@ export class Result<O, E extends ToString> {
    * @param fn Computed the default value if `Err`.
    * @returns The contained `Ok` value or computed default value.
    */
-  unwrapOrElse(fn: (error: Readonly<E>) => O): O {
-    return this.match({
-      ok: (value) => value,
-      err: (error) => fn(error),
-    });
-  }
+  unwrapOrElse(fn: (error: Readonly<E>) => O): O;
 
   /**
    * Converts from `Result<O, E>` to `Option<O>`. Converts self into an
@@ -127,12 +85,7 @@ export class Result<O, E extends ToString> {
    *
    * @returns The converted option.
    */
-  ok(): Option<O> {
-    return this.match({
-      ok: (value) => Some(value),
-      err: () => None,
-    });
-  }
+  ok(): Option<O>;
 
   /**
    * Converts from `Result<O, E>` to `Option<E>`. Converts self into an
@@ -140,12 +93,7 @@ export class Result<O, E extends ToString> {
    *
    * @returns The converted option.
    */
-  err(): Option<E> {
-    return this.match({
-      ok: () => None,
-      err: (error) => Some(error),
-    });
-  }
+  err(): Option<E>;
 
   /**
    * Maps a `Result<O, E>` to `Result<T, E>` by applying a function to a
@@ -156,12 +104,7 @@ export class Result<O, E extends ToString> {
    * @param fn The mapping function.
    * @returns The mapped result.
    */
-  map<T extends ToString>(fn: (value: Readonly<O>) => T): Result<T, E> {
-    return this.match({
-      ok: (value) => Ok(fn(value)),
-      err: (error) => Err(error),
-    });
-  }
+  map<T extends ToString>(fn: (value: Readonly<O>) => T): Result<T, E>;
 
   /**
    * Returns the provided default (if `Err`), or applies a function to the
@@ -171,12 +114,7 @@ export class Result<O, E extends ToString> {
    * @param fn The mapping function if `Ok`.
    * @returns The mapped result.
    */
-  mapOr<T>(defaultValue: T, fn: (value: Readonly<O>) => T): T {
-    return this.match({
-      ok: (value) => fn(value),
-      err: () => defaultValue,
-    });
-  }
+  mapOr<T>(defaultValue: T, fn: (value: Readonly<O>) => T): T;
 
   /**
    * Maps a `Result<O, E>` to `T` by applying fallback function default to a
@@ -192,12 +130,7 @@ export class Result<O, E extends ToString> {
   mapOrElse<T>(
     defaultFn: (error: Readonly<E>) => T,
     fn: (value: Readonly<O>) => T,
-  ): T {
-    return this.match({
-      ok: (value) => fn(value),
-      err: (error) => defaultFn(error),
-    });
-  }
+  ): T;
 
   /**
    * Maps a `Result<O, E>` to `Result<O, F>` by applying a function to a
@@ -209,12 +142,7 @@ export class Result<O, E extends ToString> {
    * @param fn Error mapping function.
    * @returns The mapped result.
    */
-  mapErr<T extends ToString>(fn: (error: Readonly<E>) => T): Result<O, T> {
-    return this.match({
-      ok: (value) => Ok(value),
-      err: (error) => Err(fn(error)),
-    });
-  }
+  mapErr<T extends ToString>(fn: (error: Readonly<E>) => T): Result<O, T>;
 
   /**
    * Returns the contained `Ok` value.
@@ -229,14 +157,7 @@ export class Result<O, E extends ToString> {
    * @throws Throws if the value is an `Err`, with a panic message including the
    * passed message, and the content of the `Err`.
    */
-  expect(message: string): O {
-    return this.match({
-      ok: (value) => value,
-      err: (error) => {
-        throw new Error(`${message}: ${error.toString()}`);
-      },
-    });
-  }
+  expect(message: string): O;
 
   /**
    * Returns the contained `Err` value.
@@ -247,12 +168,163 @@ export class Result<O, E extends ToString> {
    * @throws Throws if the value is an `Ok`, with an error message including the
    * passed message, and the content of the `Ok`.
    */
-  expectErr(message: string): E {
-    return this.match({
-      ok: () => {
-        throw new Error(`${message}`);
-      },
-      err: (error) => error,
-    });
+  expectErr(message: string): E;
+}
+
+export class OkImpl<O> implements BaseResult<O, never> {
+  constructor(public value: O) {}
+
+  isOk(): this is Ok<O> {
+    return true;
+  }
+
+  isOkAnd(fn: (value: Readonly<O>) => boolean): this is Ok<O> {
+    return fn(this.value);
+  }
+
+  isErr(): this is Err<never> {
+    return false;
+  }
+
+  isErrAnd(_fn: (error: Readonly<never>) => boolean): this is Err<never> {
+    return false;
+  }
+
+  match<T>(matcher: { ok: (value: Readonly<O>) => T }): T {
+    return matcher.ok(this.value);
+  }
+
+  unwrap(): O {
+    return this.expect('called `Result.unwrap()` on an `Err` value');
+  }
+
+  unwrapErr(): never {
+    return this.expectErr('called `Result.unwrapErr()` on an `Ok` value');
+  }
+
+  unwrapOr(_defaultValue: O): O {
+    return this.value;
+  }
+
+  unwrapOrElse(_fn: (error: Readonly<never>) => O): O {
+    return this.value;
+  }
+
+  ok(): Option<O> {
+    return Some(this.value);
+  }
+
+  err(): None {
+    return None;
+  }
+
+  map<T extends ToString>(fn: (value: Readonly<O>) => T): Result<T, never> {
+    return Ok(fn(this.value));
+  }
+
+  mapOr<T>(_defaultValue: T, fn: (value: Readonly<O>) => T): T {
+    return fn(this.value);
+  }
+
+  mapOrElse<T>(
+    _defaultFn: (error: Readonly<never>) => T,
+    fn: (value: Readonly<O>) => T,
+  ): T {
+    return fn(this.value);
+  }
+
+  mapErr<T extends ToString>(_fn: (error: Readonly<never>) => T): Result<O, T> {
+    return Ok(this.value);
+  }
+
+  expect(_message: string): O {
+    return this.value;
+  }
+
+  expectErr(message: string): never {
+    throw new Error(`${message}`);
   }
 }
+
+export const Ok = <O>(value: O) => new OkImpl(value);
+export type Ok<O> = OkImpl<O>;
+
+class ErrImpl<E extends ToString> implements BaseResult<never, E> {
+  constructor(public error: E) {}
+
+  isOk(): this is Ok<never> {
+    return false;
+  }
+
+  isOkAnd(_fn: (value: never) => boolean): this is Ok<never> {
+    return false;
+  }
+
+  isErr(): this is Err<E> {
+    return true;
+  }
+
+  isErrAnd(fn: (error: Readonly<E>) => boolean): this is Err<E> {
+    return fn(this.error);
+  }
+
+  match<T, K>(matcher: { err: (error: Readonly<E>) => K }): T | K {
+    return matcher.err(this.error);
+  }
+
+  unwrap(): never {
+    return this.expect('called `Result.unwrap()` on an `Err` value');
+  }
+
+  unwrapErr(): E {
+    return this.expectErr('called `Result.unwrapErr()` on an `Ok` value');
+  }
+
+  unwrapOr<T>(defaultValue: T): T {
+    return defaultValue;
+  }
+
+  unwrapOrElse<T>(fn: (error: Readonly<E>) => T): T {
+    return fn(this.error);
+  }
+
+  ok(): Option<never> {
+    return None;
+  }
+
+  err(): Option<E> {
+    return Some(this.error);
+  }
+
+  map<T extends ToString>(_fn: (value: never) => T): Result<never, E> {
+    return Err(this.error);
+  }
+
+  mapOr<T>(defaultValue: T, _fn: (value: never) => T): T {
+    return defaultValue;
+  }
+
+  mapOrElse<T>(
+    defaultFn: (error: Readonly<E>) => T,
+    _fn: (value: never) => T,
+  ): T {
+    return defaultFn(this.error);
+  }
+
+  mapErr<T extends ToString>(fn: (error: Readonly<E>) => T): Result<never, T> {
+    return Err(fn(this.error));
+  }
+
+  expect(message: string): never {
+    throw new Error(`${message}`);
+  }
+
+  expectErr(_message: string): E {
+    return this.error;
+  }
+}
+
+export const Err = <E extends ToString>(error: E) => new ErrImpl(error);
+export type Err<E extends ToString> = ErrImpl<E>;
+
+export type Result<O, E extends ToString> = Ok<O> | Err<E>;
