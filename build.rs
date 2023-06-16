c3ce8554311dc33ac19e4abf2d298ca5e32ac8cc
Natalie Klestrup Röijezon <nat@nullable.se>
2023-06-16 15:22:17 +0200

    Add Dockerfile

--- build.rs
+++ build.rs
@@ -5,6 +5,8 @@ use gobuild::BuildMode;
 const ENV_GO_HELM_WRAPPER: &str = "GO_HELM_WRAPPER";
 
 fn main() {
+    // cgo requires an explicit dependency on libresolv on some platforms (such as Red Hat Enterprise Linux 8 and derivatives)
+    println!("cargo:rustc-link-lib=resolv");
     println!("cargo:rerun-if-env-changed={ENV_GO_HELM_WRAPPER}");
     match env::var(ENV_GO_HELM_WRAPPER) {
         Ok(go_helm_wrapper) => {
