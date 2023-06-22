5b318893b9afd73e5b4611975f485395ab45619e
Techassi <git@techassi.dev>
2023-06-22 17:21:42 +0200

    Continue transfer layer

--- src/xfer/cache.rs
+++ src/xfer/cache.rs
@@ -1,8 +1,91 @@
-use std::{path::PathBuf, time::Duration};
+use std::{
+    path::PathBuf,
+    time::{Duration, SystemTimeError},
+};
+
+use snafu::{ResultExt, Snafu};
+use tokio::{fs, io};
 
 use crate::constants::DEFAULT_CACHE_MAX_AGE;
 
-pub struct Cache {}
+pub type CacheResult<T> = Result<T, CacheError>;
+
+#[derive(Debug, Snafu)]
+pub enum CacheError {
+    #[snafu(display("io error"))]
+    CacheIoError { source: io::Error },
+
+    #[snafu(display("system time error"))]
+    SystemTimeError { source: SystemTimeError },
+
+    #[snafu(display("tried to write file with disabled cache"))]
+    WriteDisabled,
+}
+
+pub struct Cache {
+    pub(crate) settings: CacheSettings,
+}
+
+impl Cache {
+    pub fn new(settings: CacheSettings) -> Self {
+        Self { settings }
+    }
+
+    pub fn is_enabled(&self) -> bool {
+        match self.settings.backend {
+            CacheBackend::Disk { .. } => true,
+            CacheBackend::Disabled => false,
+        }
+    }
+
+    pub async fn retrieve(&self, file_name: &str) -> CacheResult<CacheStatus<String>> {
+        match &self.settings.backend {
+            CacheBackend::Disk { base_path } => {
+                let file_path = base_path.join(file_name);
+
+                if !file_path.is_file() {
+                    return Ok(CacheStatus::Miss);
+                }
+
+                let modified = file_path
+                    .metadata()
+                    .context(CacheIoSnafu {})?
+                    .modified()
+                    .context(CacheIoSnafu {})?;
+
+                let elapsed = modified.elapsed().context(SystemTimeSnafu {})?;
+
+                if elapsed > self.settings.max_age {
+                    return Ok(CacheStatus::Expired);
+                }
+
+                let content = Self::read(file_path).await?;
+                Ok(CacheStatus::Hit(content))
+            }
+            CacheBackend::Disabled => Ok(CacheStatus::Miss),
+        }
+    }
+
+    pub async fn store(&self, file_name: &str, file_content: &str) -> CacheResult<()> {
+        match &self.settings.backend {
+            CacheBackend::Disk { base_path } => {
+                let file_path = base_path.join(file_name);
+                Self::write(file_path, file_content).await
+            }
+            CacheBackend::Disabled => WriteDisabledSnafu {}.fail(),
+        }
+    }
+
+    async fn read(file_path: PathBuf) -> CacheResult<String> {
+        fs::read_to_string(file_path).await.context(CacheIoSnafu {})
+    }
+
+    async fn write(file_path: PathBuf, file_content: &str) -> CacheResult<()> {
+        fs::write(file_path, file_content)
+            .await
+            .context(CacheIoSnafu {})
+    }
+}
 
 pub enum CacheStatus<T> {
     Hit(T),
--- src/xfer/mod.rs
+++ src/xfer/mod.rs
@@ -1,8 +1,111 @@
-mod cache;
+use serde::Deserialize;
+use snafu::{ensure, ResultExt, Snafu};
+use url::Url;
 
+mod cache;
 pub use cache::*;
+
+type Result<T> = core::result::Result<T, HttpError>;
+
+#[derive(Debug, Snafu)]
+pub enum HttpError {
+    #[snafu(display("failed to extract file name from URL"))]
+    FileNameError,
+
+    #[snafu(display("cache error"))]
+    CacheError { source: CacheError },
+
+    #[snafu(display("request error"))]
+    RequestError { source: reqwest::Error },
+
+    #[snafu(display("failed to deserialize content into YAML"))]
+    YamlError { source: serde_yaml::Error },
+}
 
 pub struct HttpClient {
     pub(crate) client: reqwest::Client,
     pub(crate) cache: Cache,
+}
+
+impl HttpClient {
+    /// Creates a new [`HttpClient`] with caching capabilities.
+    pub fn new(cache_settings: CacheSettings) -> Self {
+        let cache = Cache::new(cache_settings);
+        let client = reqwest::Client::new();
+
+        Self { client, cache }
+    }
+
+    /// Retrieves plain data from the provided `url`.
+    pub async fn get_plain_data(&self, url: Url) -> Result<String> {
+        let file_name = Self::extract_file_name(&url)?;
+
+        match self
+            .cache
+            .retrieve(&file_name)
+            .await
+            .context(CacheSnafu {})?
+        {
+            CacheStatus::Hit(content) => Ok(content),
+            CacheStatus::Expired | CacheStatus::Miss => {
+                let content = self.get(url).await?;
+                self.cache
+                    .store(&file_name, &content)
+                    .await
+                    .context(CacheSnafu {})?;
+
+                Ok(content)
+            }
+        }
+    }
+
+    /// Retrieves data from the provided `url` and tries to deserialize it
+    /// into YAML.
+    pub async fn get_yaml_data<T>(&self, url: Url) -> Result<T>
+    where
+        T: for<'a> Deserialize<'a> + Sized,
+    {
+        let file_name = Self::extract_file_name(&url)?;
+
+        let content = match self
+            .cache
+            .retrieve(&file_name)
+            .await
+            .context(CacheSnafu {})?
+        {
+            CacheStatus::Hit(content) => content,
+            CacheStatus::Expired | CacheStatus::Miss => {
+                let content = self.get(url).await?;
+                self.cache
+                    .store(&file_name, &content)
+                    .await
+                    .context(CacheSnafu {})?;
+
+                content
+            }
+        };
+
+        serde_yaml::from_str(&content).context(YamlSnafu {})
+    }
+
+    /// Internal call which executes a HTTP GET request to `url`.
+    async fn get(&self, url: Url) -> Result<String> {
+        let req = self.client.get(url).build().context(RequestSnafu {})?;
+        let result = self.client.execute(req).await.context(RequestSnafu {})?;
+
+        result.text().await.context(RequestSnafu {})
+    }
+
+    /// Returns the last URL path segment as the filename. It ensures that the
+    /// last segment contains at least one dot.
+    fn extract_file_name(url: &Url) -> Result<String> {
+        let segment = url
+            .path_segments()
+            .ok_or(HttpError::FileNameError)?
+            .last()
+            .ok_or(HttpError::FileNameError)?;
+
+        ensure!(segment.contains('.'), FileNameSnafu {});
+        Ok(segment.to_string())
+    }
 }
