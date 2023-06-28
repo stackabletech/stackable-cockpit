09d0cc786e982425364911513f7cf6222e9ee80f
Natalie Klestrup Röijezon <nat@nullable.se>
2023-06-28 15:23:20 +0200

    Delete colour definitions that Uno already provides

--- web/uno.config.ts
+++ web/uno.config.ts
@@ -3,11 +3,6 @@ import { defineConfig, presetUno } from 'unocss';
 export default defineConfig({
   theme: {
     colors: {
-      'gray-400': '#9CA3AF',
-      'gray-600': '#4B5563',
-      'gray-700': '#374151',
-      'gray-800': '#1F2937',
-      'gray-900': '#111827',
       stbluelight: '#1880BD',
       stbluedark: '#0B689F',
     },
