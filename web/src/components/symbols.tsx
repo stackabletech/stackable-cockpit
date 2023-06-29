a416a5f665fb8c936bc2b97937718c87f7aeabb0
Natalie Klestrup Röijezon <nat@nullable.se>
2023-06-29 10:32:28 +0200

    Ignore innerhtml false positive

--- web/src/components/symbols.tsx
+++ web/src/components/symbols.tsx
@@ -5,6 +5,8 @@ interface FeatherSymbolProps {
 }
 const FeatherSymbol = (props: FeatherSymbolProps) => {
   const icon = () => featherIcons[props.icon];
+  // Icon contents are provided statically by feather, and not influenced by user input
+  // eslint-disable-next-line solid/no-innerhtml
   return <svg {...icon().attrs} innerHTML={icon().contents} />;
 };
 
