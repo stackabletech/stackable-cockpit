44f75dc26f72eac1acb63588efc08a9c72e261ae
Techassi <git@techassi.dev>
2023-07-06 11:45:06 +0200

    Add ctl README gen hook

--- bins/stackablectl/src/cli/mod.rs
+++ bins/stackablectl/src/cli/mod.rs
@@ -93,11 +93,11 @@ to provide multiple additional stack files.")]
 Releases are loaded in the following order: Remote (default) release file,
 custom release files provided via the 'STACKABLE_RELEASE_FILES' environment
 variable, and lastly release files provided via the '-r/--release-file'
-argument(s). If there are releases with the same name, the last stack definition
-will be used.
+argument(s). If there are releases with the same name, the last release
+definition will be used.
 
-Use \"stackablectl -r path/to/realeases1.yaml -r path/to/realeases2.yaml [OPTIONS] <COMMAND>\"
-to provide multiple additional stack files.")]
+Use \"stackablectl -r path/to/releases1.yaml -r path/to/releases2.yaml [OPTIONS] <COMMAND>\"
+to provide multiple additional release files.")]
     pub release_files: Vec<String>,
 
     /// Provide a custom Helm stable repository URL
