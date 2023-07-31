7aaad6a97093b983e4aed3471318a65b26295046
Natalie Klestrup Röijezon <nat@nullable.se>
2023-07-31 16:41:21 +0200

    Redirect from home page to stacklets view

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,15 +2,11 @@
 import '@unocss/reset/sanitize/sanitize.css';
 import 'virtual:uno.css';
 import { render } from 'solid-js/web';
-import { Route, Router, Routes } from '@solidjs/router';
+import { Navigate, Route, Router, Routes } from '@solidjs/router';
 import { Header } from './components/header';
 import { StackletConnectionDetails } from './pages/stacklets/connect';
 import { Stacklets } from './pages/stacklets/list';
 import { LoginPageOr } from './pages/login';
-
-const Home = () => {
-  return <>lorem ipsum dolor sit amet</>;
-};
 
 const App = () => {
   return (
@@ -24,7 +20,7 @@ const App = () => {
               component={StackletConnectionDetails}
             />
             <Route path='/stacklets' component={Stacklets} />
-            <Route path='/' component={Home} />
+            <Route path='/' component={() => <Navigate href='/stacklets' />} />
           </Routes>
         </div>
       </LoginPageOr>
