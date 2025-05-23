abc56b93cd8b3c16e30ff4d28af9c97684029568
xeniape <xenia.fischer@stackable.tech>
2025-05-23 14:32:17 +0200

    remove progress bar color

--- rust/stackable-cockpit/src/lib.rs
+++ rust/stackable-cockpit/src/lib.rs
@@ -12,10 +12,8 @@ pub mod utils;
 pub mod xfer;
 
 pub static PROGRESS_BAR_STYLE: LazyLock<ProgressStyle> = LazyLock::new(|| {
-    ProgressStyle::with_template(
-        "{span_child_prefix}Progress: {wide_bar:.magenta/cyan} {pos}/{len}",
-    )
-    .expect("valid progress template")
+    ProgressStyle::with_template("{span_child_prefix}Progress: {wide_bar} {pos}/{len}")
+        .expect("valid progress template")
 });
 
 pub static PROGRESS_SPINNER_STYLE: LazyLock<ProgressStyle> = LazyLock::new(|| {
