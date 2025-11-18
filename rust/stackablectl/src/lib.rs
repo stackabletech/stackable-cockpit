ac129fe7f471824d6de1a56adccd9e2977c2d3ed
Techassi <git@techassi.dev>
2025-11-18 16:58:01 +0100

    chore: Add built

--- rust/stackablectl/src/lib.rs
+++ rust/stackablectl/src/lib.rs
@@ -4,3 +4,13 @@ pub mod cmds;
 pub mod constants;
 pub mod output;
 pub mod utils;
+
+pub mod built_info {
+    use std::{str::FromStr, sync::LazyLock};
+
+    include!(concat!(env!("OUT_DIR"), "/built.rs"));
+
+    pub static PKG_SEMVER: LazyLock<semver::Version> = LazyLock::new(|| {
+        semver::Version::from_str(PKG_VERSION).expect("must be a valid semantic version")
+    });
+}
